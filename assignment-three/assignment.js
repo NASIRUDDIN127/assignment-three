4754593756f668d3a3452145d3b9d5cf2be64839
nasir uddin <nasirbdctg@yahoo.com>
2021-01-22 12:35:34 +0000

    allcode

---
+++ assignment-three/assignment.js
@@ -0,0 +1,49 @@
+
+
+//project one kilometerToMeter
+function kilometerToMeter(kilometer){
+    var meter = kilometer*1000;
+    return meter;
+}
+var result = kilometerToMeter(213);
+console.log(result);
+
+
+//project three hotelCost
+
+function hotelCost(day) {
+    var cost = 0;
+    if (day <= 10) {
+        cost = day * 100;
+    }
+    else if (day <= 20) {
+        var firstPart = 10 * 100;
+        var remaining = day - 10;
+        var secondPart = remaining * 80;
+        cost = firstPart + secondPart;
+    }
+    else {
+        var firstPart = 10 * 100;
+        var secondPart = 10 * 80;
+        var remaining = day - 20;
+        var thirdPart = remaining * 50;
+        cost = firstPart + secondPart + thirdPart;
+    }
+    return cost;
+}
+var count = hotelCost(32);
+console.log(count);
+
+
+//project four megaFriend
+
+function megaFriend(strs) {
+    let longest = '';
+    for (let i = 0; i < strs.length; i++) {
+    if (strs[i].length > longest.length)
+    longest = strs[i];
+    }
+    return longest;
+    }
+    var result = megaFriend(['kalam', 'naz', 'hero', 'shahin']);
+    console.log(result);
